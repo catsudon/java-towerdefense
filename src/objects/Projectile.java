e55637f9865b4e8a118c3e5c8b57acb9ca75e6e0
Sathana Laolugsanalerd <52562864+mark48853@users.noreply.github.com>
2023-05-20 22:20:16 +0700

    fix projectile

--- src/objects/Projectile.java
+++ src/objects/Projectile.java
@@ -36,48 +36,51 @@ public class Projectile {
 	}
 	
 	public void move() {
-		if(enemy != null && !enemy.isAlive()) {
-			enemy = null;
-		}
-		if(enemy != null) {
-			int xDiff = (int) (this.getPos().getX() - (enemy.getX() + 16));
-			int yDiff = (int) (this.getPos().getY() - (enemy.getY() + 16));
-			int totalDist = Math.abs(xDiff) + Math.abs(yDiff);
-			if(Math.abs(xDiff) <= 16 || Math.abs(yDiff) <= 16) {
-				enemy = null;
-				this.pos = this.pos.add(xSpeed, ySpeed);
-				return ;
-			}
-			
-			// Percent
-			float xPer = (float) Math.abs(xDiff) / totalDist;
-			float yPer = 1.0f - xPer;
+        if(enemy != null && !enemy.isAlive()) {
+            enemy = null;
+            this.active = false;
+            return ;
+        }
+        if(enemy != null) {
+            int xDiff = (int) (this.getPos().getX() - (enemy.getX() + 16));
+            int yDiff = (int) (this.getPos().getY() - (enemy.getY() + 16));
+            int totalDist = Math.abs(xDiff) + Math.abs(yDiff);
+            if(Math.hypot(xDiff, yDiff) <= 16) {
+                enemy = null;
+                this.pos = this.pos.add(xSpeed, ySpeed);
+                return ;
+            }
+            
+            // Percent
+            float xPer = (float) Math.abs(xDiff) / (totalDist + 0.001f);
+            float yPer = 1.0f - xPer;
 
-			this.xSpeed = xPer * utilities.Constants.Projectiles.getConstantSpeed(projectileType);
-			this.ySpeed = yPer * utilities.Constants.Projectiles.getConstantSpeed(projectileType);
-			
-			if (this.getPos().getX() > enemy.getX()) {
-				this.xSpeed *= -1;
-			}
-			if (this.getPos().getY() > enemy.getY()) {
-				this.ySpeed *= -1;
-			}
-			
-			this.rotationAngle = 0;
+            this.xSpeed = xPer * utilities.Constants.Projectiles.getConstantSpeed(projectileType);
+            this.ySpeed = yPer * utilities.Constants.Projectiles.getConstantSpeed(projectileType);
+            
+            if (this.getPos().getX() > enemy.getX()) {
+                this.xSpeed *= -1;
+            }
+            if (this.getPos().getY() > enemy.getY()) {
+                this.ySpeed *= -1;
+            }
+            
+            this.rotationAngle = 0;
 
-			if(projectileType == CROISSANT) {
-				float arctan = (float) Math.atan((float) yDiff / xDiff);
-				this.rotationAngle = (float) Math.toDegrees(arctan);
-				if (xDiff < 0) {
-					this.rotationAngle += 180;
-				}
-			}
-		}
-		if(xSpeed == 0 && ySpeed == 0) {
-			this.active = false;
-		}
-		this.pos = this.pos.add(xSpeed, ySpeed);
-	}
+            if(projectileType == CROISSANT) {
+                float arctan = (float) Math.atan((float) yDiff / xDiff);
+                this.rotationAngle = (float) Math.toDegrees(arctan);
+                if (xDiff < 0) {
+                    this.rotationAngle += 180;
+                }
+            }
+        }
+        if(xSpeed == 0 && ySpeed == 0) {
+            this.active = false;
+        }
+        this.pos = this.pos.add(xSpeed, ySpeed);
+
+    }
 
 	public Point2D getPos() {
 		return pos;
